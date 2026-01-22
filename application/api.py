4739218749eabc46fa5716bc0d91f3b746000e12
kirs1748 <kirs.professionnel@gmail.com>
2026-01-22 21:11:35 +0100

    mise en place des grilles

--- application/api.py
+++ application/api.py
@@ -1,9 +1,11 @@
 import requests
+import json
+
 
 class ApiClient :
 
     #marche seulement en partage de coo
     def get(self, url):
         r = requests.get(url)
-        return r.status_code, r.text
+        return r.status_code, r.content, r.headers, r.cookies
         
--- application/app.py
+++ application/app.py
@@ -7,6 +7,7 @@ class App :
     def __init__(self, root):
         self.root = root
         self.root.title("Nom de l'application")
+        self.root.state('zoomed')
 
         # Backend
         self.api = ApiClient()
@@ -19,36 +20,65 @@ class App :
         self.build_ui()
 
     def build_ui(self):
-        # Titre de l'application
-        self.app_title = Label( self.root , text = "Nom de l'application" , fg = "white", bg="#595657", font = ("Arial", 30))
-        self.app_title.pack(side=TOP)
+        # Configuration de la grille générale
+        self.root.grid_rowconfigure(0, weight=0)  # header
+        self.root.grid_rowconfigure(1, weight=0)  # requete
+        self.root.grid_rowconfigure(2, weight=0)  # paramètres
+        self.root.grid_rowconfigure(3, weight=0)  # réponse
+        self.root.grid_columnconfigure(0, weight=1)
+
+        # Frame pour l'en-tête
+        self.header_frame = Frame( self.root, bg = "#FFFFFF" )
+        self.header_frame.grid(row=0, column=0, sticky="nsew")
+        self.menubar = Menu( self.header_frame )
+        self.menubar.add_command( label = "Automatisation", command= lambda: print("Automatisation clicked"))
+        self.root.config( menu = self.menubar )
+        self.app_title = Label( self.header_frame , text = "Nom de l'application" , fg = "black", bg="#FFFFFF", font = ("Impact", 30))
+        self.app_title.pack( side=TOP, padx=20, pady=20 )
 
         # Frame pour les requêtes
-        self.frame_requete = Frame( self.root, bg = "#FFFFFF" )
-        self.frame_requete.pack()
+        self.frame_requete = Frame( self.root, bg = "#00FF22", pady=10 )
+        self.frame_requete.grid(row=1, column=0, sticky="nsew")
+        #Configuration de la grille de la frame requête
+        self.frame_requete.grid_columnconfigure(0, weight=1)
+        self.frame_requete.grid_columnconfigure(1, weight=1)
+        self.frame_requete.grid_columnconfigure(2, weight=1)
 
         # Choix de la requête
-        self.request_scrollbar = Scrollbar(self.frame_requete,bg="#FFFFFF")
-        self.request_scrollbar.pack( side=LEFT, fill=Y )
-        self.request_listbox = Listbox( self.frame_requete, fg = "black", bg="#FFFFFF", font = ("Arial", 14), width=10, height=5, yscrollcommand=self.request_scrollbar.set )
-        self.request_listbox.pack( side=LEFT, fill=BOTH, padx=10, pady=10 )
-        self.request_listbox.insert(END, "GET")
-        self.request_listbox.insert(END, "POST")
-        self.request_listbox.insert(END, "PUT")
-        self.request_listbox.insert(END, "DELETE")
-
-
+        self.choix_requete = StringVar(value="GET")
+        self.request_menu = OptionMenu( self.frame_requete , self.choix_requete , "GET" , "POST" , "PUT" , "DELETE" )
+        self.request_menu.grid(row=0, column=0, padx=3, pady=10, sticky="e")
         # Champ d'entrée pour l'URL
         self.url_entry = Entry( self.frame_requete , fg = "black", bg="#B13636", font = ("Arial", 20), width=40 )
-        self.url_entry.pack( side=LEFT, padx=10, pady=10 )
-
+        self.url_entry.grid(row=0, column=1, padx=10, pady=10)
+        # Bouton pour envoyer la requête
         self.send_button = Button(self.frame_requete , text = "send" , fg = "black", bg="#D3D3D3", font = ("Arial", 20), command=lambda: self.send_request())
-        self.send_button.pack( side=LEFT, padx=10, pady=10 )
+        self.send_button.grid(row=0, column=2, padx=10, pady=10,sticky="w")
+
+
+        #Frame pour les paramètres
+        self.frame_params = Frame( self.root, bg = "#FF00D9" )
+        self.frame_params.grid(row=2, column=0, sticky="nsew")
+
+        params_label = Label( self.frame_params , text = "Parameters:" , fg = "black", bg="#005EFF", font = ("Arial", 20))
+        params_label.pack(side=TOP, padx=10, pady=10 )
+
+
+
+        #Frame pour la réponse
+        self.frame_reponse = Frame( self.root, bg = "#7C6363" )
+        self.frame_reponse.grid(row=3, column=0, sticky="nsew")
+
+
+        response_label = Label( self.frame_reponse , text = "Response:" , fg = "black", bg="#FFFFFF", font = ("Arial", 20))
+        response_label.pack( side=TOP, padx=10, pady=10 )
+        self.response_text = Message( self.frame_reponse , text = "" , fg = "black", bg="#FFFFFF", font = ("Arial", 14), width=1000 )
+        self.response_text.pack( side=TOP, padx=10, pady=10 )
 
 
 
     def send_request(self):
-        method = self.request_listbox.get(ACTIVE)
+        method = self.choix_requete.get()
         if method == "GET":
             self.send_get_request()
         elif method == "POST":
@@ -62,8 +92,13 @@ class App :
     def send_get_request(self):
         url = self.url_entry.get()
         print("metode GET")
-        status_code, response_text = self.api.get(url)
+        status_code, response_content , response_headers, response_cookies = self.api.get(url)
         print(f"Status Code: {status_code}")
+        #print(f"Response Text: {response_content}")
+        print(f"Response Headers: {response_headers}")
+        #print(f"Response JSON: {response_json}")
+        #print(f"Response Cookies: {response_cookies}")
+
 
     def send_post_request(self):
         print("metode POST")
